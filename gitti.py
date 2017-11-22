35d4db6a048de340d2d947ab58008ed537f3dc59
Jozsa Vilmos <vilixjo@gmail.com>
2017-11-22 09:52:42 +0100

    edgy joke :)

--- gitti.py
+++ gitti.py
@@ -1,2 +1,7 @@
-def hello():
+def introduce():
     print("Hello gittie")
+
+def edgy_joke():
+    print("DO you wanna hear a joke?")
+    print("Why don’t black people take cruises? \n They fell for that trick once already!")
+edgy_joke()
